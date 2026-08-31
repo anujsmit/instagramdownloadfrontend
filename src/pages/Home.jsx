17bc793a33acf58f4bd56f80134f199c1a565903
anujsmit <anujkattel16@gmail.com>
2026-08-31 21:01:40 +0530

    hello

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,7 +11,7 @@ function Home() {
     const [videoSize, setVideoSize] = useState("");
     const [showResult, setShowResult] = useState(false);
 
-    const API_URL = "http://127.0.0.1:5050";
+    const API_URL = "https://apiinsta.anujkattel.com.np/";
 
     useEffect(() => {
         document.title =
